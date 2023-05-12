289ef03460cad8a31611ed6226e30ed4395cb4dc
russellbanares322 <banaresrussell11@gmail.com>
2023-05-12 21:57:21 +0800

    feat: add discount text indicator in product page

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import poloImage from "../../assets/sampleProduct.png";
 
 const Product = () => {
   return (
-    <div className="flex justify-center mt-28 bg-white rounded-xl shadow-xl p-5 border border-zinc-200">
+    <div className="flex relative justify-center mt-28 bg-white rounded-xl shadow-xl p-5 border border-zinc-200">
+      <div className="absolute left-3 top-3 bg-slate-500 text-white px-3">
+        <h3 className="text-xs">30% less</h3>
+      </div>
+      <div className="bg-white absolute left-[4.89rem] h-3 w-3 top-[0.87rem] rotate-[42deg]" />
+      <div className="bg-white absolute left-[0.35rem] h-3 w-3 top-[0.87rem] rotate-[42deg]" />
       <div>
         <img className="w-56 object-cover h-auto" src={poloImage} />
       </div>
